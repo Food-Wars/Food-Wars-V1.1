530710fd5b980dca4a4abb5bef0aa5f12a42e68a
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2025-09-02 16:44:45 -0400

    Inital commit

---
+++ game_engines/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 19:05:02 2024
+
+"""
+
+#from .player_engine import *
+from .enitites import *
+from.library_lisences import *
+version = "1.0"
+print('Food Wars Game Engine Version ' + version)
+print('-Entites Engine Version 1.0')
+print('-Lisence Info 1.0')
